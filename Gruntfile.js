87848df00c0ca7700573eb3df4dd24400ffe5c97
Rustam Yusupov <rustam.yusupov@gmail.com>
2016-04-08 18:30:53 +0500

    поправил задачу копирования

--- Gruntfile.js
+++ Gruntfile.js
@@ -145,7 +145,7 @@ module.exports = function(grunt) {
     watch: {
       html: {
         files: ['src/**/*.html'],
-        tasks: ['htm'],
+        tasks: ['copy:htm'],
         options: {
           spawn: false,
           livereload: true
@@ -153,7 +153,7 @@ module.exports = function(grunt) {
       },
       style: {
         files: ['src/sass/**/*.scss'],
-        tasks: ['style'],
+        tasks: ['copy:style'],
         options: {
           spawn: false,
           livereload: true
@@ -161,7 +161,7 @@ module.exports = function(grunt) {
       },
       images: {
         files: ['src/img/**/*.{png,jpg,gif,svg}'],
-        tasks: ['img'],
+        tasks: ['copy:img'],
         options: {
           spawn: false,
           livereload: true
