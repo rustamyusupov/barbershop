07ca5418575e3ba09d2679e0337be12527e0a224
Rustam Yusupov <rustam.yusupov@gmail.com>
2016-05-03 20:17:52 +0200

    исправил баги safari, сделал резину для фото

--- src/js/news.js
+++ src/js/news.js
@@ -10,18 +10,11 @@ function showAllNews(event) {
   for (var i = 0; i < news.length; i++) {
     news[i].style.display = 'flex';
 
-    if (viewportWidth < 768) {
-      news[i].style.borderBottom = '1px solid #e5ddd5';
-    }
-
     if ( i === 1 ) {
       news[i].style.marginBottom = 'auto';
     }
 
-    if ( i === news.length - 1 ) {
-      news[i].style.border = 'none';
-      news[i].style.marginBottom = "5px";
-    }
+    news[i].style.border = 'none';
 
   }
 
